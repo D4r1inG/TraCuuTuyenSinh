0a70fd643cb762247fbc0df4b10177fa8b6d0647
D4rl1nG <quangaobn@gmail.com>
2022-06-16 17:26:21 +0700

    change name

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React, { Component, useEffect, useState } from 'react'
 import axios, { Axios } from 'axios';
-import ExcelPage from './Component/ExcelPage';
+import ExcelPage from './Page/ExcelPage';
 import 'antd/dist/antd.css';
 
 function App() {
